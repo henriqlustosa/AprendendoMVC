3c9a5a4382ebe0b2217b982a5d58b6717a3ddf86
Henrique Lustosa <henriqlustosa@gmail.com>
2021-05-17 17:14:18 -0300

    Finalizado o projeto para entender o padrão MVC

---
+++ Alura.ListaLeitura.App/Controllers/CadastroController.cs
@@ -0,0 +1,33 @@
+﻿
+using Alura.ListaLeitura.App.Models;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public string Incluir(Livro livro)
+        {
+            var repo = new LivroRepositorioCSV();
+            repo.Incluir(livro);
+            return "O livro foi adicionado com sucesso";
+        }
+
+        public IActionResult ExibeFormulario()
+        {
+            var html = new ViewResult { ViewName = "formulario" };
+
+            return html;
+        }
+
+    }
+}
